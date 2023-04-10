602b270a4322f00fb04f54f1d1809cf5b553da8e
Watheq Attrah <novaatrah@gmail.com>
2023-04-09 22:15:43 -0700

    complete

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+
+
+const element=document.getElementById("main");
+element.remove("main");
+let newHeader = document.createElement("H1");
+newHeader.id=("victory");
+
+newHeader.textContent=("Watheq Attrah is the champion");
+newHeader.innerHTML=("victory","Watheq Attrah is the champion");
